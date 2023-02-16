a31b0177f8373a3b9018647999827d2c68849b60
8-sided-oregon <mansamham@protonmail.ch>
2023-02-15 20:02:27 -0500

    woops

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -173,8 +173,6 @@ public class Robot extends TimedRobot {
       );
     }
 
-    logger.info("transgender >w>");
-
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     compressor = new Compressor(1, PneumaticsModuleType.REVPH);
