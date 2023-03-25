a3caea2f25572ffbabfaac39c47137e2dad5b3f6
Aidan10650 <aidanvanduyne@gmail.com>
2023-03-24 22:22:45 -0400

    made finite state machine class and used it in drop state

--- src/main/java/frc/robot/states/auto/DropState.java
+++ src/main/java/frc/robot/states/auto/DropState.java
@@ -1,5 +1,6 @@
 package frc.robot.states.auto;
 
+import frc.robot.FiniteStateMachine;
 import frc.robot.RobotStateManager;
 import frc.robot.State;
 import frc.robot.subsystems.TurretvatorSubsystem;
@@ -8,14 +9,42 @@ import frc.robot.subsystems.TurretvatorSubsystem.ElevatorStage;
 public class DropState implements State{
     private String name, parent;
     private TurretvatorSubsystem tvSub;
-    private int state;
-    private long dropStartTime;
-    private long loopStartTime;
+    private FiniteStateMachine finiteStateMachine = new FiniteStateMachine();
 
     public DropState(String name, String parent){
         this.name = name;
         this.parent = parent;
         tvSub = TurretvatorSubsystem.getInstance();
+        
+        finiteStateMachine.addState(() -> {}, () -> {
+            return finiteStateMachine.getTimeSinceStartOfCurrentState() > 500;
+        })
+        
+        .addState(() -> {
+            tvSub.elevatorSet(3.6); 
+        }, () -> {
+            return 
+                tvSub.isExtended() ||
+                finiteStateMachine.getTimeSinceStartOfCurrentState() > 1000;
+        })
+        
+        .addState(() -> {}, () -> {
+            return finiteStateMachine.getTimeSinceStartOfCurrentState() > 1000;
+        })
+
+        .addState(() -> {
+            tvSub.setGripper(false);
+        }, () -> {
+            return finiteStateMachine.getTimeSinceStartOfCurrentState() > 1000;
+        })
+
+        .addState(() -> {
+            tvSub.elevatorSet(ElevatorStage.DOWN);
+        }, () -> {
+            return 
+                tvSub.isExtended() ||
+                finiteStateMachine.getTimeSinceStartOfCurrentState() > 500;
+        });
     }
 
     @Override
@@ -30,9 +59,8 @@ public class DropState implements State{
 
     @Override
     public void Enter() {
-        state = 0;
+        
         tvSub.setGripper(true);
-        loopStartTime = System.currentTimeMillis();
         System.out.println("entered" + name);
     }
 
@@ -43,45 +71,8 @@ public class DropState implements State{
 
     @Override
     public void Periodic(RobotStateManager rs) {
-        switch(state) {
-            case 0:
-                if(System.currentTimeMillis() - loopStartTime > 500) state++;
-                break;
-            case 1:// extend elavator
-                System.out.println("Entered HIGH");
-                tvSub.elevatorSet(3.6); 
-                if(tvSub.isExtended()){
-                    loopStartTime = System.currentTimeMillis();
-                    state++;
-                }
-                break;
-            case 2:
-                System.out.println("entered wait");
-                if(System.currentTimeMillis() - loopStartTime > 1000) {
-                    state++;
-                    dropStartTime = System.currentTimeMillis();
-                }
-                break;
-            case 3:// drop game element
-                System.out.println("Entered Gripper open");
-                tvSub.setGripper(false);
-                if(System.currentTimeMillis() - dropStartTime > 500) {
-                    state++;
-                    dropStartTime = System.currentTimeMillis();
-                }
-                break;
-            case 4:// retract elevator
-                tvSub.elevatorSet(ElevatorStage.DOWN);
-                if(System.currentTimeMillis() - dropStartTime > 500) state++;
-                break;
-            default:
-                rs.setState("path");
-                break;
-            
+        if(!finiteStateMachine.periodic()){
+            rs.setState("path");
         }
-        /**
-         * set outtake to open
-         * exit state when finished opening
-         */
     }
 }
